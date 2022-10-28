1cfd10143e45c01b527478961729d3f925ff6332
pritika-rana <pritika@Pritikas-MacBook-Pro.local>
2022-10-28 14:28:24 +0530

    first commit

---
+++ src/main/java/com/example/googlelogin1/GoogleLogin1Application.java
@@ -0,0 +1,13 @@
+package com.example.googlelogin1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleLogin1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleLogin1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/googlelogin1/GoogleLogin1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.googlelogin1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleLogin1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
